0308b867d89535b22334f1804b28efa503a15242
Gustavo Collioni <kautz.collioni@gmail.com>
2025-11-23 12:10:42 -0300

    Commit PB t12

--- pages/Application.py
+++ pages/Application.py
@@ -13,11 +13,11 @@ st.set_page_config(
 )
 
 hide_st_style = '''
-
+<style>
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 header {visibility: hidden;}
-
+</style>
 '''
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
